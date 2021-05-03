660b74abc782f272497620b1a5de6dc39be45a8f
Kamil <you@example.com>
2021-05-03 19:10:44 +0200

    bug

--- src/utils/service.ts
+++ src/utils/service.ts
@@ -9,18 +9,7 @@ export const addQuizApi = async (auth, values) => {
     const resp = await axios.post("/api/quiz", values, { headers: header });
     return resp;
   } catch (error) {
-    let e = error;
-    if (error.response) {
-      e = error.response.data; // data, status, headers
-      if (error.response.data && error.response.data.error) {
-        e = error.response.data.error; // my app specific keys override
-      }
-    } else if (error.message) {
-      e = error.message;
-    } else {
-      e = "Unknown error occured";
-    }
-    return e;
+    throw error;
   }
 };
 
